0cfd77e1822ad4cfbe5df49bddc2e2c8c0633077
Michelle Brytowski <mn.brytowski@gmail.com>
2019-03-10 20:53:29 -0600

    update build

--- build/precache-manifest.55d20a713a82b9384efcb60ec84da17a.js
+++ build/precache-manifest.55d20a713a82b9384efcb60ec84da17a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/clicky-game/static/js/runtime~main.7985e359.js"
   },
   {
-    "revision": "f43c8508bdf02ec29deb",
-    "url": "/clicky-game/static/js/main.f43c8508.chunk.js"
+    "revision": "79efaf61a405807db0aa",
+    "url": "/clicky-game/static/js/main.79efaf61.chunk.js"
   },
   {
     "revision": "134243148c8e9bc01730",
     "url": "/clicky-game/static/js/2.13424314.chunk.js"
   },
   {
-    "revision": "f43c8508bdf02ec29deb",
+    "revision": "79efaf61a405807db0aa",
     "url": "/clicky-game/static/css/main.f09ae437.chunk.css"
   },
   {
-    "revision": "fb10a4354eae052b32f7fed746aa1a95",
+    "revision": "ebd31195ccad27b141e1222fadaff591",
     "url": "/clicky-game/index.html"
   }
 ];
